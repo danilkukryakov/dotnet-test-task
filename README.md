c82cbc8686fa9103b303ea1ac4754439705a24c7
Danil Kukryakov <danil.kukryakov@saritasa.com>
2022-12-17 14:52:50 +0500

    Project initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotnet-test-task
+
+Secrets sharing app
---
+++ src/SecretsSharing.Web/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecretsSharing.Web.Controllers;
+
+/// <summary>
+/// Weather forecast controller.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    /// <summary>
+    /// Get weather forecast.
+    /// </summary>
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
